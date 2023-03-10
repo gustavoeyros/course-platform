cc7a81d0109bed3d0aa155b490094e65c1d244cd
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-03-10 16:49:51 -0300

    feat: field validations and register

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,20 +1,22 @@
 import { Request, Response } from "express";
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export default class UserController {
   static async register(req: Request, res: Response) {
     const { name, email, password, confirmpassword } = req.body;
-    if (!name) {
-      return res.status(422).json({ message: "O nome é obrigatório!" });
-    }
-    if (!email) {
-      return res.status(422).json({ message: "O Email é obrigatório!" });
-    }
-    if (!password) {
-      return res.status(422).json({ message: "A senha é obrigatória!" });
-    }
+    const requiredFields = ["name", "email", "password"];
+    //verificar se os campos foram preenchidos
+    requiredFields.some((field) => {
+      if (!req.body[field]) {
+        return res
+          .status(422)
+          .json({ message: `O campo ${field} é obrigatório` });
+      }
+    });
+
     if (password !== confirmpassword) {
-      return res.status(422).json({ message: "As senhas não batem!" });
+      return res.status(422).json({ message: "As senhas não conferem!" });
     }
 
     //verificar se já existe uma acc
@@ -22,6 +24,23 @@ export default class UserController {
 
     if (userExist) {
       return res.status(422).json({ message: "O usuário já existe!" });
+    }
+
+    //criação da senha criptografada
+    const salt = await bcrypt.genSalt(12);
+    const passHash = await bcrypt.hash(password, salt);
+
+    //registro do usuário
+    const user = new User({
+      name,
+      email,
+      password,
+    });
+    try {
+      await user.save();
+      res.status(201).json({ message: "Usuário criado com sucesso!" });
+    } catch (error) {
+      res.status(500).json({ message: error });
     }
   }
 }
