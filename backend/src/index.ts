6a0af06ebcd073675accf52d5da8f7af966db786
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-03-14 11:39:24 -0300

    feat: cors added to integration with frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import UserRoutes from "./routes/UserRoutes";
 import CourseRoutes from "./routes/CourseRoutes";
 import { run } from "./db/conn";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/users", UserRoutes);
 app.use("/course", CourseRoutes);
