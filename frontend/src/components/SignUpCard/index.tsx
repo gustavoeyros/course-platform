05f32863962712fa552bb2f0176ee7785500e6dd
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-03-15 10:47:43 -0300

    feat: initial validations for sign up page

--- frontend/src/components/SignUpCard/index.tsx
+++ frontend/src/components/SignUpCard/index.tsx
@@ -2,24 +2,77 @@ import { Container } from "../WelcomeCard/styled";
 import InputForm from "../InputForm";
 import ButtonForm from "../ButtonForm";
 import { useNavigate } from "react-router-dom";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Card, SignInContianer } from "./styled";
 
 const SignUpCard = () => {
   const navigate = useNavigate();
+
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const confirmPasswordRef = useRef<HTMLInputElement>(null);
+
+  const [nameError, setNameError] = useState<boolean | null>(null);
+  const [emailError, setEmailError] = useState<boolean | null>(null);
+  const [passwordError, setPasswordError] = useState<boolean | null>(null);
+  const [confirmPasswordError, setConfirmPasswordError] = useState<
+    boolean | null
+  >(null);
+
+  const submitHandler = () => {
+    //validations
+    nameRef.current?.value.length !== 0
+      ? setNameError(true)
+      : setNameError(false);
+
+    emailRef.current?.value.length !== 0
+      ? setEmailError(true)
+      : setEmailError(false);
+
+    passwordRef.current?.value.length !== 0
+      ? setPasswordError(true)
+      : setPasswordError(false);
+
+    confirmPasswordRef.current?.value.length !== 0 ||
+    confirmPasswordRef.current?.value === passwordRef.current?.value
+      ? setConfirmPasswordError(true)
+      : setConfirmPasswordError(false);
+  };
+
   return (
     <Container>
       <Card>
         <h1>Sign up!</h1>
-        <InputForm type="text" placeholder="name" />
-        <InputForm type="email" placeholder="e-mail" />
-        <InputForm type="password" placeholder="password" />
-        <InputForm type="password" placeholder="confirm password" />
+        <InputForm
+          inputRef={nameRef}
+          type="text"
+          placeholder="name"
+          hasEmpty={nameError}
+        />
+        <InputForm
+          inputRef={emailRef}
+          type="email"
+          placeholder="e-mail"
+          hasEmpty={emailError}
+        />
+        <InputForm
+          inputRef={passwordRef}
+          type="password"
+          placeholder="password"
+          hasEmpty={passwordError}
+        />
+        <InputForm
+          inputRef={confirmPasswordRef}
+          type="password"
+          placeholder="confirm password"
+          hasEmpty={confirmPasswordError}
+        />
         <SignInContianer>
           Already have an account?{" "}
           <span onClick={() => navigate("/")}>Sign Up</span>
         </SignInContianer>
-        <ButtonForm onClick={() => ""} />
+        <ButtonForm onClick={submitHandler} />
       </Card>
     </Container>
   );
