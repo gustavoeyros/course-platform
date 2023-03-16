da0fc78ec20fafc7c737b7f7e595d3bb28c185cd
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-03-16 14:16:20 -0300

    feat: initial display informations at home

--- frontend/src/components/HomeCourses/index.tsx
+++ frontend/src/components/HomeCourses/index.tsx
@@ -1,10 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+interface ICourses {
+  description: string;
+  image_url: string;
+}
 
 const HomeCourses = () => {
   const token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MGM4OTU0ZWZhMWE5NjRlOGFmYzU4YyIsImlhdCI6MTY3ODk4MDQ0NH0.ClAOm6_dA0qJcIGDniplBrNmfpjirVqdP4qn6WqnreA";
 
+  const [courses, setCourses] = useState<ICourses[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>();
+
   const getAllCourses = () => {
+    setIsLoading(true);
     fetch("http://localhost:3000/course/all", {
       method: "GET",
       headers: {
@@ -16,16 +25,32 @@ const HomeCourses = () => {
         console.log(res);
         return res.json();
       })
-      .then((data) => console.log(data));
+      .then((data) => {
+        setCourses(data.message);
+        setIsLoading(false);
+        console.log(courses);
+      });
   };
 
   useEffect(() => {
     getAllCourses();
   }, []);
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        courses.map((data) => (
+          <div>
+            <h1>{data.description}</h1>
+            <p>
+              <img src={data.image_url} />
+            </p>
+          </div>
+        ))
+      )}
+    </>
   );
 };
 
