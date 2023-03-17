20f5e5b1a736339ea88d191f812aa3265d41d150
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-03-17 15:19:53 -0300

    feat: initial display courses enrolled

--- frontend/src/pages/MyCourses.tsx
+++ frontend/src/pages/MyCourses.tsx
@@ -1,7 +1,56 @@
 import HomeHeader from "../components/HomeHeader";
+import { useEffect, useState } from "react";
+
+interface ICourses {
+  description: string;
+}
 
 const MyCourses = () => {
-  return <HomeHeader />;
+  const [courses, setCourses] = useState<ICourses[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>();
+
+  const user = JSON.parse(localStorage.getItem("user") || "");
+  const userId = user.id;
+  const userToken = user.token;
+
+  const getMyCourses = () => {
+    setIsLoading(true);
+    fetch(`http://localhost:3000/users/mycourses/${userId}`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        Authorization: `Bearer ${userToken}`,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        return res.json();
+      })
+      .then((data) => {
+        const enrolledCourses = data.user.enrolledCourses;
+        setCourses(enrolledCourses);
+        setIsLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    getMyCourses();
+  }, []);
+
+  return (
+    <>
+      <HomeHeader />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          {courses.map((data) => (
+            <p>{data.description}</p>
+          ))}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default MyCourses;
