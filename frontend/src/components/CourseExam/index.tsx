13a8209fbadfe17cc1badcdd0e234a910f57b356
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-03-23 10:43:28 -0300

    feat: initial structure to display questions

--- frontend/src/components/CourseExam/index.tsx
+++ frontend/src/components/CourseExam/index.tsx
@@ -1,9 +1,23 @@
-import { ExamContainer } from "./styled";
+import { useState } from "react";
+import { ExamContainer, Backdrop, ExamCard } from "./styled";
 const CourseExam = () => {
+  const [showExam, setShowExam] = useState<boolean | null>(null);
   return (
-    <ExamContainer>
-      <h1>Realizar prova do curso</h1>
-    </ExamContainer>
+    <>
+      <ExamContainer>
+        <h1 onClick={() => setShowExam(true)}>Realizar prova do curso</h1>
+      </ExamContainer>
+
+      {showExam ? (
+        <Backdrop>
+          <ExamCard>
+            <span onClick={() => setShowExam(false)}>Close</span>
+          </ExamCard>
+        </Backdrop>
+      ) : (
+        ""
+      )}
+    </>
   );
 };
 
--- frontend/src/components/CourseExam/styled.ts
+++ frontend/src/components/CourseExam/styled.ts
@@ -6,3 +6,28 @@ export const ExamContainer = styled.div`
   align-items: center;
   cursor: pointer;
 `;
+
+export const Backdrop = styled.div`
+  display: flex;
+  position: fixed;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  z-index: 20;
+  background-color: rgba(0, 0, 0, 0.75);
+`;
+
+export const ExamCard = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 350px;
+  height: 350px;
+  background-color: white;
+  border-radius: 5px;
+
+  & span {
+    cursor: pointer;
+  }
+`;
