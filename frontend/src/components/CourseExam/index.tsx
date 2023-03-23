759ae50da0ac938adb8c398b6dd5b8e1ee45f3de
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-03-23 11:10:29 -0300

    feat: get questions from api

--- frontend/src/components/CourseExam/index.tsx
+++ frontend/src/components/CourseExam/index.tsx
@@ -1,7 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { ExamContainer, Backdrop, ExamCard } from "./styled";
+
+interface IQuestions {
+  answer: string;
+  options: [];
+  question: string;
+  questionId: number;
+  _id: string;
+}
+
 const CourseExam = () => {
   const [showExam, setShowExam] = useState<boolean | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean | null>(null);
+  const [questions, setQuestions] = useState<IQuestions[]>([]);
+  const { id } = useParams();
+  const user = JSON.parse(localStorage.getItem("user") || "");
+  const token = user.token;
+
+  const getCourseById = () => {
+    setIsLoading(true);
+    fetch(`http://localhost:3000/course/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        return res.json();
+      })
+      .then((data) => {
+        const questionsFromAPI = data.course.questions;
+        setQuestions(questionsFromAPI);
+        setIsLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    getCourseById();
+  }, []);
   return (
     <>
       <ExamContainer>
@@ -12,6 +51,7 @@ const CourseExam = () => {
         <Backdrop>
           <ExamCard>
             <span onClick={() => setShowExam(false)}>Close</span>
+            {isLoading ? <h1>Questões</h1> : ""}
           </ExamCard>
         </Backdrop>
       ) : (
